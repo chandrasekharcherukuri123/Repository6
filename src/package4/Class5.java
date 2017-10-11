2fd89748d0bb202ab634619b8911512e99a931af
chandrasekhar009 <31537403+chandrasekhar009@users.noreply.github.com>
2017-10-11 15:10:26 +0530

    commit

---
+++ src/package4/Class5.java
@@ -0,0 +1,20 @@
+package package4;
+
+import java.util.Scanner;
+
+public class Class5 {
+
+	public static void main(String[] args)
+	{
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter username");
+		String u=sc.nextLine();
+		System.out.println("enter password");
+		String p=sc.nextLine();
+		//commit
+		//by pull
+
+	}
+
+}
